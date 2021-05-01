16b6d3a4645b2c784b085e30d56bfa72bbf6251e
hosseind88 <hosseind88@mail.ru>
2021-05-01 18:02:05 +0430

    generate ts declaration type

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,11 @@
-declare function _exports(baseClass: string, conditionalClassesObject: object): string;
-export = _exports;
+/*!
+ * conditional-class <https://github.com/hosseind88/conditional-class>
+ *
+ * Copyright (c) 2021 Hossein Dindar.
+ * Released under the MIT License.
+ */
+interface ClassObject {
+    [key: string]: boolean;
+}
+export declare function conditionalClass(baseClass?: string, conditionalClassesObject?: ClassObject): string;
+export {};
